2427802f4155cb105970965d330e98ae6e90392a
Hano <hinsul3113@gmail.com>
2019-11-22 12:01:23 -0800

    Add React

--- src/a.js
+++ src/a.js
@@ -1,11 +1,24 @@
-import "./b";
-console.log("This is a |");
+// import "./b";
+// console.log("This is a |");
+import React from "react";
+import ReactDOM from "react-dom";
 
 import webpackLogo from "./images/webpack-logo.jpeg";
 
 document.addEventListener("DOMContentLoaded", () => {
-  const img = document.createElement("img");
-  img.src = webpackLogo;
+  const root = document.createElement("div");
 
-  document.body.append(img);
+  document.body.append(root);
+
+  ReactDOM.render(
+    <img
+      style={{
+        borderRadius: "20px",
+        border: "3px solid gray"
+      }}
+      width="400px"
+      src={webpackLogo}
+    />,
+    root
+  );
 });
